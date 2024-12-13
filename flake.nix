6b55b77fe968d6a6cee3c72f01b62e1322e1e674
William Vera <wv@linux.com>
2024-12-12 20:42:27 -0600

    allowed unfree

--- flake.nix
+++ flake.nix
@@ -23,25 +23,25 @@
         system,
         ...
       }: let
+        nixpkgsConfig = {allowUnfree = true;};
+        pkgsWithConfig = import inputs.nixpkgs {
+          inherit system;
+          config = nixpkgsConfig;
+        };
         nixvimLib = nixvim.lib.${system};
         nixvim' = nixvim.legacyPackages.${system};
         nixvimModule = {
-          inherit pkgs;
-          module = import ./config; # import the module directly
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-          extraSpecialArgs = {
-            # inherit (inputs) foo;
-          };
+          pkgs = pkgsWithConfig;
+          module = import ./config;
+          extraSpecialArgs = {};
         };
         nvim = nixvim'.makeNixvimWithModule nixvimModule;
       in {
         checks = {
-          # Run `nix flake check .` to verify that your config is not broken
           default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
         };
 
         packages = {
-          # Lets you run `nix run .` to start nixvim
           default = nvim;
         };
       };
