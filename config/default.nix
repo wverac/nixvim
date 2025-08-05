00ee8189461e584234d1a056d36b07a4b2da3880
William Vera <wv@linux.com>
2025-08-04 22:33:31 -0500

    alejandra was here!

--- config/default.nix
+++ config/default.nix
@@ -6,7 +6,7 @@
   # Helper to determine if we're on Darwin
   isDarwin = pkgs.stdenv.isDarwin;
   isLinux = pkgs.stdenv.isLinux;
-  
+
   # Common packages for all systems
   commonPackages = with pkgs; [
     # Required by telescope live grep
@@ -27,13 +27,13 @@
     #rubyfmt
     shfmt
   ];
-  
+
   # Linux-specific packages
   linuxPackages = with pkgs; [
-    wl-clipboard  # Wayland clipboard
+    wl-clipboard # Wayland clipboard
     xdg-utils
   ];
-  
+
   # Darwin-specific packages
   darwinPackages = with pkgs; [
     # macOS uses pbcopy/pbpaste for clipboard, which is built-in
@@ -51,8 +51,17 @@ in {
     ./plugs/lualine.nix
   ];
   # globals.mapleader = " "; # defined in keymaps module
-  
-  extraPackages = commonPackages 
-    ++ (if isLinux then linuxPackages else [])
-    ++ (if isDarwin then darwinPackages else []);
+
+  extraPackages =
+    commonPackages
+    ++ (
+      if isLinux
+      then linuxPackages
+      else []
+    )
+    ++ (
+      if isDarwin
+      then darwinPackages
+      else []
+    );
 }
--- config/options.nix
+++ config/options.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   config = {
     viAlias = true;
     vimAlias = true;
